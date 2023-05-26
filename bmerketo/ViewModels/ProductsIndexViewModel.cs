cbb77a178898e1b4a598458b852afd4e90713f9d
Malin-Eriksson <sangsta.malin@gmail.com>
2023-05-26 18:21:09 +0200

    design product details started

--- bmerketo/ViewModels/ProductsIndexViewModel.cs
+++ bmerketo/ViewModels/ProductsIndexViewModel.cs
@@ -1,8 +1,13 @@
 ﻿using bmerketo.Models;
+using bmerketo.Models.Entities;
 
 namespace bmerketo.ViewModels;
 
 public class ProductsIndexViewModel
 {
+	public string? Title { get; set; } = "";
+	public IEnumerable<TagEntity> ProductTags { get; set; } = new List<TagEntity>();
 	public IEnumerable<ProductModel> Products { get; set; } = new List<ProductModel>();
+
+	public bool LoadMore { get; set; } = false;
 }
